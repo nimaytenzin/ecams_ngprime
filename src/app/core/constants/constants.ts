a45cb80ba1ce444bc6d15e3a2f20336fc11d04a3
nimaytenzin <nimaytenzin@gmail.com>
2025-07-03 12:59:57 +0600

    Updates API URL to use a secure domain
    Replaces the API URL with a secure HTTPS domain to enhance security and align with the updated server configuration. Retains the ASSET_URL as is.

--- src/app/core/constants/constants.ts
+++ src/app/core/constants/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://157.10.138.104/efiling_back/api';
+export const API_URL = 'https://ecams.ddnsfree.com/efiling_back/api';
 export const ASSET_URL = 'http://localhost:3002';
